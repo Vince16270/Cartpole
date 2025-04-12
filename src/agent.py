ab5836ff8b6797800e4ac67910a4c71bf89966c3
Nima Ghafar <nimaaghafar@gmail.com>
2025-04-12 19:11:54 +0200

    agent.py changes

--- src/agent.py
+++ src/agent.py
@@ -1,111 +1,87 @@
+import random
+import pandas as pd
 import numpy as np
 
-class QLearningAgent:
-    def __init__(self, env, num_bins=6, alpha=0.1, gamma=0.99, epsilon=1.0, epsilon_min=0.01, epsilon_decay=0.995):
-        """
-        Initialiseer de Q-learning agent.
-        
-        Parameters:
-          - env: de Gymnasium-omgeving
-          - num_bins: aantal bins per dimensie voor de discretisatie van de continue state space
-          - alpha: learning rate
-          - gamma: discount factor
-          - epsilon: initiële waarde voor de epsilon-greedy strategie
-          - epsilon_min: minimumwaarde voor epsilon
-          - epsilon_decay: factor waarmee epsilon per episode afneemt
-        """
+
+class AgentBasic(object):
+    ''' Simple agent class. '''
+    def __init__(self):
+        pass
+
+    def act(self, obs):
+        ''' Based on angle, return action. '''
+        angle = obs[2]
+        return 0 if angle < 0 else 1
+
+
+class AgentRandom(object):
+    ''' Random agent class. '''
+    def __init__(self, action_space):
+        self.action_space = action_space
+
+    def act(self):
+        ''' Agent randomly chooses an action. '''
+        return self.action_space.sample()
+
+
+class QLearningAgent(object):
+    ''' Agent that can learn via Q-learning. '''
+    def __init__(self, env, alpha=0.1, epsilon=1.0, gamma=0.7):
         self.env = env
-        self.num_bins = num_bins
         self.alpha = alpha
-        self.gamma = gamma
         self.epsilon = epsilon
-        self.epsilon_min = epsilon_min
-        self.epsilon_decay = epsilon_decay
+        self.gamma = gamma
+        self.Q_table = dict()
+        self._set_seed()
 
-        # Creëer de bins om de continue observatieruimte te discretiseren
-        self.bins = self._create_bins()
+    def _set_seed(self):
+        ''' Set random seeds for reproducibility. '''
+        np.random.seed(21)
+        random.seed(21)
 
-        # Initialiseer de Q-table met de juiste dimensies:
-        # (num_bins per state-dimensie) + (aantal acties)
-        self.q_table = np.zeros(self._get_q_table_shape())
+    def build_state(self, features):
+        ''' Build state by concatenating features into int. '''
+        return int("".join(map(lambda feature: str(int(feature)), features)))
 
-    def _create_bins(self):
-        """
-        Creëer discretisatie-bins voor elke dimensie van de observatie.
-        Voor CartPole kunnen sommige grenzen oneindig zijn; vervang deze met redelijke limieten.
-        """
-        bins = []
-        low = self.env.observation_space.low.copy()
-        high = self.env.observation_space.high.copy()
-        # Voor CartPole: pas grenzen aan voor cart velocity en pole velocity
-        low[1] = -3.0
-        high[1] = 3.0
-        low[3] = -3.0
-        high[3] = 3.0
-
-        for i in range(len(low)):
-            # Creëer bins met numpy.linspace en sluit de uiterste grenzen uit
-            bins.append(np.linspace(low[i], high[i], self.num_bins + 1)[1:-1])
-        return bins
-
-    def _get_q_table_shape(self):
-        """
-        Bepaal de shape van de Q-table op basis van het aantal bins per state-dimensie
-        en het aantal acties in de omgeving.
-        """
-        dims = [self.num_bins] * self.env.observation_space.shape[0] + [self.env.action_space.n]
-        return tuple(dims)
-    
-    def discretize(self, observation):
-        """
-        Converteer een continue observatie naar een discrete staat.
-        
-        Parameters:
-          - observation: de continue observatie (numpy array)
-        
-        Returns:
-          - Een tuple met discrete indices voor elke dimensie
-        """
-        discretized = []
-        for i, val in enumerate(observation):
-            discretized.append(np.digitize(val, self.bins[i]))
-        return tuple(discretized)
+    def create_state(self, obs):
+        ''' Create a discrete state from observation. '''
+        cart_position_bins = pd.cut([-2.4, 2.4], bins=10, retbins=True)[1][1:-1]
+        pole_angle_bins = pd.cut([-2, 2], bins=10, retbins=True)[1][1:-1]
+        cart_velocity_bins = pd.cut([-1, 1], bins=10, retbins=True)[1][1:-1]
+        angle_rate_bins = pd.cut([-3.5, 3.5], bins=10, retbins=True)[1][1:-1]
+        state = self.build_state([
+            np.digitize(x=[obs[0]], bins=cart_position_bins)[0],
+            np.digitize(x=[obs[1]], bins=pole_angle_bins)[0],
+            np.digitize(x=[obs[2]], bins=cart_velocity_bins)[0],
+            np.digitize(x=[obs[3]], bins=angle_rate_bins)[0]
+        ])
+        return state
 
     def choose_action(self, state):
-        """
-        Kies een actie volgens de epsilon-greedy strategie.
-        
-        Parameters:
-          - state: de huidige discrete staat
-        
-        Returns:
-          - Een actie (int) op basis van exploratie of exploitatie
-        """
-        if np.random.random() < self.epsilon:
-            return self.env.action_space.sample()  # Exploratie: kies een willekeurige actie
+        ''' Choose an action using epsilon-greedy. '''
+        if random.random() < self.epsilon:
+            return self.env.action_space.sample()
         else:
-            return np.argmax(self.q_table[state])  # Exploitatie: kies de beste bekende actie
-    
-    def update(self, state, action, reward, next_state, done):
-        """
-        Voer de Q-learning update uit voor een gegeven (state, action)-paar.
-        
-        Parameters:
-          - state: de huidige discrete staat
-          - action: de gekozen actie (int)
-          - reward: de ontvangen beloning
-          - next_state: de volgende discrete staat
-          - done: boolean die aangeeft of de episode is beëindigd
-        """
-        if done:
-            td_target = reward
-        else:
-            best_next_action = np.argmax(self.q_table[next_state])
-            td_target = reward + self.gamma * self.q_table[next_state + (best_next_action,)]
-        td_error = td_target - self.q_table[state + (action,)]
-        self.q_table[state + (action,)] += self.alpha * td_error
+            max_Q = self.get_maxQ(state)
+            actions = [a for a, q in self.Q_table[state].items() if q == max_Q]
+            return random.choice(actions) if actions else self.env.action_space.sample()
 
-    def decay_epsilon(self):
-        """Verlaag epsilon zodat de agent in latere episodes meer gaat exploiteren."""
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+    def create_Q(self, state, valid_actions):
+        ''' Initialize Q values if state not in Q table. '''
+        if state not in self.Q_table:
+            self.Q_table[state] = {action: 0.0 for action in valid_actions}
+
+    def get_maxQ(self, state):
+        ''' Return the max Q value for a state. '''
+        return max(self.Q_table[state].values())
+
+    def learn(self, state, action, prev_reward, prev_state, prev_action):
+        ''' Q-learning update step. '''
+        self.Q_table[prev_state][prev_action] = (
+            (1 - self.alpha) * self.Q_table[prev_state][prev_action] +
+            self.alpha * (prev_reward + self.gamma * self.get_maxQ(state))
+        )
+
+    def decay_epsilon(self, decay=0.995, min_epsilon=0.01):
+        ''' Decay epsilon for exploration/exploitation tradeoff. '''
+        self.epsilon = max(self.epsilon * decay, min_epsilon)
